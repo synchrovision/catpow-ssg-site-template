bb8985fba5cf92ba177bcb6e781f16e307d992ee
synchrovision <hatano@catpow.info>
2026-03-31 12:22:00 +0900

    edit 📝 update common.js

--- _tmpl/js/common/index.jsx
+++ _tmpl/js/common/index.jsx
@@ -1,9 +1,12 @@
-import {SiteCloak} from './SiteCloak.jsx';
-import {SiteHeader} from './SiteHeader.jsx';
-import {SiteFooter} from './SiteFooter.jsx';
+import { ready } from "catpow/util";
+import { createRoot } from "react-dom/client";
 
-window.addEventListener('DOMContentLoaded',function(){
-	ReactDOM.render(<SiteCloak/>,document.getElementById('SiteCloak'));
-	ReactDOM.render(<SiteHeader/>,document.getElementById('SiteHeader'));
-	ReactDOM.render(<SiteFooter/>,document.getElementById('SiteFooter'));
+import { SiteCloak } from "./SiteCloak.jsx";
+import { SiteHeader } from "./SiteHeader.jsx";
+import { SiteFooter } from "./SiteFooter.jsx";
+
+ready(() => {
+	createRoot(document.getElementById("SiteCloak")).render(<SiteCloak />);
+	createRoot(document.getElementById("SiteHeader")).render(<SiteHeader />);
+	createRoot(document.getElementById("SiteFooter")).render(<SiteFooter />);
 });
